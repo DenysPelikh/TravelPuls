5587326d3060d5f05a1946265ff046e32701fce3
Denys Pelikh <dpelikh@exadel.com>
2019-08-16 17:00:36 +0300

    Added Points

---
+++ TravelPuls.Database/dbo/Tables/Points.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Points]
+(
+	[Id] BIGINT IDENTITY (1, 1) NOT NULL,
+	[RouteId] INT NOT NULL,
+	[PreviousId] BIGINT,
+	[Latitude] FLOAT NOT NULL,
+	[Longitude] FLOAT NOT NULL,
+	CONSTRAINT [PK_Points] PRIMARY KEY CLUSTERED ([ID] ASC),
+	CONSTRAINT [FK_Points_PreviousId] FOREIGN KEY ([PreviousId]) REFERENCES Points([Id])
+) ON [PRIMARY];
