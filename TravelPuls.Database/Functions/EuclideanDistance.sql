35f113f12885cda73b946b6b82e4508194891fef
Denys Pelikh <dpelikh@exadel.com>
2019-09-02 17:41:56 +0300

    Added DistanceView

---
+++ TravelPuls.Database/Functions/EuclideanDistance.sql
@@ -0,0 +1,15 @@
+﻿CREATE FUNCTION [dbo].[EuclideanDistance]
+(
+	@x1 float,
+	@y1 float,
+	@x2 float,
+	@y2 float
+)
+RETURNS float WITH SCHEMABINDING
+AS BEGIN
+    DECLARE @Result float
+
+    SET @Result = ROUND(SQRT(POWER(@x1 - @x2, 2) + POWER(@y1 - @y2, 2)), 2)
+
+    RETURN @Result
+END
---
+++ TravelPuls.Database/Views/DistanceView.sql
@@ -0,0 +1,43 @@
+﻿CREATE VIEW [dbo].[DistanceView]
+AS
+	WITH CTE_Points_Destination
+	AS (
+		select P1.Id
+		from [dbo].[Points] as P1
+		left join [dbo].[Points] as P2 on P1.Id = P2.PreviousId
+		where P2.Id IS NULL
+	),
+
+	CTE_Points_Branch
+	AS (
+		SELECT 
+			P1.Id as 'P1',
+			P2.Id as 'P2',
+			P1.PreviousId as 'P1.PreviousId',
+			P2.PreviousId as 'P2.PreviousId',
+			row_number() over (order by P1.Id) as Branch,
+			0 as order_level,
+			P1.RouteId,
+			dbo.[EuclideanDistance](P1.Latitude, P1.Longitude, P2.Latitude, P2.Longitude) as Distance
+		FROM [dbo].[Points] as P1
+			inner join [dbo].[Points] as P2 on P1.Id = P2.PreviousId and  P1.RouteId = P2.RouteId
+			inner join CTE_Points_Destination as Destinations on Destinations.Id = P2.Id
+		UNION ALL
+		SELECT 
+			P1.Id as 'P1',
+			P2.Id as 'P2',
+			P1.PreviousId as 'P1.PreviousId',
+			P2.PreviousId as 'P2.PreviousId',
+			Branch,
+			order_level + 1 as order_level,
+			P1.RouteId,
+		[dbo].[EuclideanDistance](P1.Latitude, P1.Longitude, P2.Latitude, P2.Longitude) as Distance
+		FROM [dbo].[Points] as P1
+			inner join [dbo].[Points] as P2 on P1.Id = P2.PreviousId and P1.RouteId = P2.RouteId
+			inner join CTE_Points_Branch as CTE on CTE.P1 = P2.Id
+	)
+
+	SELECT Branch, RouteId, MIN(P1) as StartPointId, MAX(P2) as EndPointId, 
+	Sum(Distance) as Distance
+	FROM CTE_Points_Branch
+	GROUP BY Branch, RouteId
